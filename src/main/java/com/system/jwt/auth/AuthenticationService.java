244d34c711f301bb04ca098a0ca883446255dff9
Anderson Portes <portsanderson@gmail.com>
2023-01-03 13:42:54 -0300

    Initial Commit

---
+++ src/main/java/com/system/jwt/auth/AuthenticationService.java
@@ -0,0 +1,49 @@
+package com.system.jwt.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.system.jwt.config.JwtService;
+import com.system.jwt.user.AppUser;
+import com.system.jwt.user.Role;
+import com.system.jwt.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
+
+  public AuthenticationResponse register(RegisterRequest request) {
+    var user = AppUser.builder()
+        .firstname(request.getFirstname())
+        .lastname(request.getLastname())
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .role(Role.USER)
+        .build();
+    userRepository.save(user);
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
+  }
+
+  public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+    var user = userRepository.findByEmail(request.getEmail())
+        .orElseThrow();
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
+  }
+
+}
---
+++ src/main/java/com/system/jwt/user/Role.java
@@ -0,0 +1,6 @@
+package com.system.jwt.user;
+
+public enum Role {
+  USER,
+  ADMIN
+}
